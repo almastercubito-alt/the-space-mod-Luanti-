c7378c2c82edf2758dfe614bfe66a8807c90b08b
almastercubito-alt <almastercubito@gmail.com>
2026-06-06 15:36:51 -0400

    first alpha version files added

---
+++ the_space_mod/init.lua
@@ -0,0 +1,24 @@
+core.register_node("the_space_mod:regolith", {
+    description = "Lunar Regolith",
+    tiles = {"regolith.png"},
+    groups = {craky = 4}
+
+})
+
+core.register_node("the_space_mod:lunar_gravel", {
+    description = "Lunar Gravel",
+    tiles = {"lunargravel.png"},
+    groups = {craky = 4}
+
+})
+
+core.register_node("the_space_mod:lunar_stone", {
+    description = "Lunar Stone",
+    tiles = {"lunarstone.png"},
+    groups = {craky = 4}
+})
+
+core.register_node("the_space_mod:station_center", {
+    description = "Station Center",
+    tiles = {"station_center.png"}
+})
