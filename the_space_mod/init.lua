8a12d6828ea496dd0994b49df630b678e59ec586
almastercubito-alt <almastercubito@gmail.com>
2026-06-06 17:29:49 -0400

    Add files via upload
    updating the source code to the last release

--- the_space_mod/init.lua
+++ the_space_mod/init.lua
@@ -1,3 +1,4 @@
+--blocks
 core.register_node("the_space_mod:regolith", {
     description = "Lunar Regolith",
     tiles = {"regolith.png"},
@@ -21,4 +22,28 @@ core.register_node("the_space_mod:lunar_stone", {
 core.register_node("the_space_mod:station_center", {
     description = "Station Center",
     tiles = {"station_center.png"}
+})
+--alias
+core.register_alias("regolith", "the_space_mod:regolith")
+core.register_alias("station_center", "the_space_mod:station_center")
+core.register_alias("lunar_gravel", "the_space_mod:lunar_gravel")
+core.register_alias("lunar_stone", "the_space_mod:lunar_stone")
+core.register_alias("gas_tank", "the_space_mod:gas_tank")
+core.register_alias("electric_crafter", "the_space_mod:electric_crafter")
+-- items
+core.register_craftitem("the_space_mod:gas_tank", {
+    description = "Gas Tank",
+    inventory_image = "the_space_mod_gas_tank.png"
+})
+
+--machines
+core.register_node("the_space_mod:electric_crafter", {
+    description = "Electric Crafter",
+    tiles = {"electric_crafter_up.png", -- y+
+    "electric_crafter_sides.png", -- y-
+    "electric_crafter_sides.png", -- x+
+    "electric_crafter_sides.png", -- x-
+    "electric_crafter_sides.png", -- z+
+    "electric_crafter_front.png"},-- z-
+    groups = {craky=3, stone=1}
 })
